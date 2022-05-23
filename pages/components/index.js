269182de1d9302a246edecd9276123cdde96cdac
Tyler Woolcott <Tyler.woolcott@gmail.com>
2022-05-23 19:35:16 +0100

    update pages components

--- pages/components/index.js
+++ pages/components/index.js
@@ -3,6 +3,7 @@ export { default as Companies } from './Companies';
 export { default as Funds } from './Funds';
 export { default as HomeHero } from './HomeHero';
 export { default as FeaturedPosts } from './FeaturedPosts';
-// export { default as FasanaraWords } from './FasanaraHero';
+export { default as FasanaraHero } from './FasanaraHero';
 export { default as Performance } from './Performance';
+export { default as useTypedWord } from './useTypedWord';
 export { default as Footer } from './Footer';
